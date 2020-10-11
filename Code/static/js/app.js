2a47bed54d76a8b5e5e0edad58db7d690905f13d
KamarRashid <k.abdur.rashid@mail.utoronto.ca>
2020-10-11 14:09:23 -0400

    added bubble chart and page initilization fn

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -14,14 +14,21 @@ function subjectDropDown() {
 
     });
 };
+// Call function to create the dropdown list
 subjectDropDown()
 
-// Initialize the page with a default plot
+// Initialize the page with a default plot of first subject in data sample
 function init() {
-    var subject = d3.select('#selDataset').property('value');
-    console.log(subject)
-    createBarChart(subject);
+    // Read in data
+    d3.json('Data/samples.json').then((data) => {
+        var firstSubject = data.names[0];
+
+        createBarChart(firstSubject);
+        createBubbleChart(firstSubject);
+        createDemographics(firstSubject);
+    })
 };
+// Call function to intialize dashboard
 init()
 
 // On change to the DOM function - get new subject ID when changed
@@ -29,6 +36,8 @@ function optionChanged(newSample) {
     var dropDownMenu = d3.select('#selDataset');
     var subject = dropDownMenu.property('value');
     createBarChart(subject);
+    createBubbleChart(subject);
+    createDemographics(subject);
 };
 
 // Horizontal bar chart function
@@ -63,12 +72,68 @@ function createBarChart(subject) {
 
         // Bar layout
         var layout = {
-            titile: `Subject ${subject}: Top 10 OTUs` 
+            // title: `Subject ${subject}: Top 10 OTUs` 
+            margin: {
+                t: 0,
+                l: 150
+            },
         }
 
         // Update bar plot
         Plotly.newPlot('bar', trace1, layout);
-        // Plotly.restyle("bar", trace1, layout);
+        // Plotly.restyle('bar', trace1, layout);
+    });
+}
+
+// Bubble chart function
+function createBubbleChart(subject) {
+    // Read in data
+    d3.json('Data/samples.json').then((data) => {
+        var samples = data.samples;
+        var filterSubject = samples.filter(sample => sample.id == subject);
+        var results = filterSubject[0];
+
+        // Create variables for bubble chart
+        var sampleValues = results.sample_values;
+        var otuIDs = results.otu_ids;
+        var otuLabels = results.otu_labels;
+        
+        console.log(sampleValues)
+        console.log(otuIDs)
+        console.log(otuLabels)
+
+        // Bubble data trace
+        var trace2 = [{
+            mode: 'markers',
+            x: otuIDs,
+            y: sampleValues,
+            text: otuLabels,
+            marker: {
+                size: sampleValues,
+                color: otuIDs
+            }
+        }];
+
+        // Bubble layout
+        var layout = {
+            // title: `Subject ${subject}: OTUs`,
+            xaxis: { title: "UTO-ID" },
+            // showlegend: true
+        }
+
+        // Update bubble plot
+        Plotly.newPlot('bubble', trace2, layout);
+        // Plotly.restyle('bubble', trace2, layout);
+    });
+}
+
+// Demographic info function
+function createDemographics(subject) {
+    // Read in data
+    d3.json('Data/samples.json').then((data) => {
+        var samples = data.samples;
+        var filterSubject = samples.filter(sample => sample.id == subject);
+        var results = filterSubject[0];
     });
 }
 // createBarChart('940')
